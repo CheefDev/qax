75251d7fffd6610e533c4e7bf1f3048e99282b75
Victor Faria <IRMAODOJOJO2@GMAIL.COM>
2026-03-02 13:02:30 -0300

    Update FileReplacer.py
    Atualizada a formatação do QR Code criado pela API

--- Resources/FileReplacer.py
+++ Resources/FileReplacer.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 tag = str(sys.argv[1])
 tag = str(tag)
 # The URL you want to make a GET request to
-url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + tag
+url = "http://api.qrserver.com/v1/create-qr-code/?size=150x150&margin=25&ecc=M&charset-target=UTF-8&data=" + tag
 
 # Send the GET request and store the response
 response = requests.get(url)
@@ -41,5 +41,6 @@ shutil.copy(source_file, destination_file)
 # Option 2: Move and replace (deleting the original source file)
 # os.replace() is a more robust way to move/rename and replace
 #os.replace(source_file, destination_file) 
+
 
 print("Arquivo Rodado!")
